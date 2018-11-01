b054f430cddce739ca499cbdd178bedcc7c36ed8
Drake Bott <bott.da@gmail.com>
2018-11-01 09:08:02 -0600

    Create store, wrap App with BrowserRouter and Provider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {rootReducer} from './reducers';
+import {BrowserRouter} from 'react-router-dom';
 import App from './components/App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
