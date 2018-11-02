8600d4ffe6ea7d6780213a75f17cf36bbc4e33f7
Drake Bott <bott.da@gmail.com>
2018-11-01 21:08:33 -0600

    Style Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './Home.css';
 
-export default function App() {
+export default function Home() {
   return (
-    <div className="Home">
-      <div className="title-container">
-        <div className="title-box">
-          <h1 className="Home-title">GLOBE SQUASHERS</h1>
-          <p className="Home-subheader">
-            HELP PROVE THE HUBBLE TELESCOPE IS A LIE
-          </p>
+    <div className='Home'>
+      <div className='Home-title-page'>
+        <div className='title-container'>
+          <div className='title-box'>
+            <h1 className='Home-title'>GLOBE SQUASHERS</h1>
+            <p className='Home-subheader'>
+              HELP PROVE THE HUBBLE TELESCOPE IS A LIE
+            </p>
+          </div>
+          <button className='scroll-to-info'></button>
         </div>
       </div>
+      <div className='Home-info-page' />
     </div>
   );
 }
