d53c98f18de8010ab6aaed9ef566c0c7287844b1
Drake Bott <bott.da@gmail.com>
2018-11-01 20:12:07 -0600

    Add home route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,30 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import {connect} from 'react-redux';
+import {Switch, Route} from 'react-router';
 import {fetchImages} from '../../thunks/fetchImages';
+import Home from '../Home';
 import './App.css';
 
 export function App({fetchImages, images, page}) {
   const [initialLoad, setInitialLoad] = useState(false);
   useEffect(() => {
-    if(!initialLoad) {
+    if (!initialLoad) {
       fetchImages(page);
       setInitialLoad(true);
     }
   });
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <Switch>
+        <Route to='/' component={Home}/>
+      </Switch>
     </div>
   );
 }
 
 const mapStateToProps = state => ({
   page: state.API.page,
-  images: state.images
+  images: state.images,
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchImages: (page) => dispatch(fetchImages(page))
+  fetchImages: page => dispatch(fetchImages(page)),
 });
 
 export default connect(
