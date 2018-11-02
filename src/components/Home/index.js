7afc3fdff6cf357b2540b920c8be2f58daaa8765
Drake Bott <bott.da@gmail.com>
2018-11-01 20:12:20 -0600

    Create and style home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+
+export default function App() {
+  return (
+    <div className='Home'>
+      <div className='title-container'>
+        <div className='title-box'>
+          <h1 className='Home-title'>GLOBE SQUASHERS</h1>
+          <p className='Home-subheader'>HELP PROVE THE HUBBLE TELESCOPE IS A LIE</p>
+        </div>
+      </div>
+    </div>
+  )
+}
