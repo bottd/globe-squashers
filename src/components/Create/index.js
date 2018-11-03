aed2a0a30b66a44def8f53a0c52463ff3f2e5de5
Drake Bott <bott.da@gmail.com>
2018-11-02 20:49:50 -0600

    Write imageID selecting logic for create

--- src/components/Create/index.js
+++ src/components/Create/index.js
@@ -1,18 +1,30 @@
 import React, {useState, useEffect} from 'react';
+import {NavLink, Redirect} from 'react-router-dom';
 import {fetchImages} from '../../thunks/fetchImages';
 import {connect} from 'react-redux';
 import './Create.css';
 
-export function Create({fetchImages, images, page}) {
+export function Create(props) {
   const [isLoaded, setLoaded] = useState(false);
+  const [imageID, setImageID] = useState(0);
+  const {fetchImages, images, page} = props;
   useEffect(() => {
     if (!isLoaded) {
       fetchImages(page);
       setLoaded(true);
     }
+    if (!imageID && images.length) {
+      const rand = Math.floor(Math.random() * images.length) + 1;
+      setImageID(images[rand].id);
+    }
   });
 
-  return <div className="Create">Create Page</div>;
+  return (
+    <div className="Create">
+      <NavLink to='/'>HOME</NavLink>
+      <NavLink to='/share'>VIEW OTHERS</NavLink>
+    </div>
+  );
 }
 
 export const mapStateToProps = state => ({
--- src/components/Create/tests/Create.test.js
+++ src/components/Create/tests/Create.test.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {MemoryRouter} from 'react-router-dom';
+import {shallow, mount} from 'enzyme';
 import {Create} from '../';
 
 describe('Create', () => {
   describe('Create component', () => {
     let wrapper;
     beforeEach(() => {
-      wrapper = mount(
-        <Create fetchImages={jest.fn()} page={2} images={[]} />,
+      wrapper = shallow(
+        <MemoryRouter>
+          <Create fetchImages={jest.fn()} page={2} images={[]} />,
+        </MemoryRouter>
       );
     });
 
