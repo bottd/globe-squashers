8f71d686a589c11c74c541e18d2a0759d0b668e7
Drake Bott <bott.da@gmail.com>
2018-11-03 10:43:49 -0600

    Create nav bar, style and test

---
+++ src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './Nav.css';
+
+export default function Nav() {
+  return (
+    <div className='Nav'>
+      <NavLink to='/' className='NavLink'>HOME</NavLink>
+      <NavLink to='/create' className='NavLink'>CREATE</NavLink>
+      <NavLink to='/browse' className='NavLink'>BROWSE</NavLink>
+    </div>
+  );
+}
