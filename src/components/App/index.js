64798c4b165bc3537473936fc0a4d93992d3029b
Drake Bott <bott.da@gmail.com>
2018-11-03 16:58:39 -0600

    Add Browse route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route, withRouter} from 'react-router';
 import Nav from '../Nav';
 import Home from '../Home';
+import Browse from '../Browse';
 import Create from '../Create';
 import './App.css';
 
@@ -11,6 +12,7 @@ export function App() {
       <Nav/>
       <Switch>
         <Route exact path="/create" component={Create} />
+        <Route exact path="/browse" component={Browse} />
         <Route exact path="/" component={Home} />
       </Switch>
     </div>
