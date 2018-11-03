b724560b564e849d02569f4a3c1f5c5d3f3a942c
Drake Bott <bott.da@gmail.com>
2018-11-02 19:04:08 -0600

    Add Create component, add create route

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Switch, Route} from 'react-router';
+import {Switch, Route, withRouter} from 'react-router';
 import {fetchImages} from '../../thunks/fetchImages';
 import Home from '../Home';
+import Create from '../Create';
 import './App.css';
 
 export class App extends Component {
   componentDidMount() {
-    this.props.fetchImages();
-  }
+    this.props.fetchImages(); }
 
   render() {
     return (
       <div className="App">
         <Switch>
-          <Route to="/" component={Home} />
+          <Route exact path="/create" component={Create} />
+          <Route path="/" component={Home} />
         </Switch>
       </div>
     );
@@ -30,7 +31,7 @@ export const mapDispatchToProps = dispatch => ({
   fetchImages: page => dispatch(fetchImages(page)),
 });
 
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps,
-)(App);
+)(App));
