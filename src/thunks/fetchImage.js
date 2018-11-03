0b9f9d87dd124549439a8eda67c6e6bde607f970
Drake Bott <bott.da@gmail.com>
2018-11-02 22:10:41 -0600

    Write and test fetchImage

--- src/thunks/fetchImage.js
+++ src/thunks/fetchImage.js
@@ -1,5 +1,5 @@
 import {isLoading, hasErrored} from '../actions/apiActions';
-import {setImage} from '../actions/imagesActions';
+import {setImage} from '../actions/imageActions';
 
 export const fetchImage = id => {
   return async dispatch => {
@@ -7,8 +7,8 @@ export const fetchImage = id => {
     try {
       const response = await fetch(`http://hubblesite.org/api/v3/image/${id}`);
       const image = await response.json();
-      const selectImage = image.map(file => file.file_url.includes('.jpg'));
-      dispatch(setImage(selectImage[0]));
+      const findUrl = image.find(file => file.file_url.includes('.jpg'));
+      dispatch(setImage(findUrl.file_url));
       dispatch(isLoading(false));
     } catch (error) {
       dispatch(hasErrored(error.message));
