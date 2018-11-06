9ed22c32450022dd25ec3daf8f56e75016dcad13
Drake Bott <bott.da@gmail.com>
2018-11-06 12:05:57 -0700

    Update Create snapshot

--- src/components/Create/tests/Create.test.js
+++ src/components/Create/tests/Create.test.js
@@ -5,14 +5,20 @@ import {Create} from '../';
 
 describe('Create', () => {
   describe('Create component', () => {
-    let wrapper, create;
+    let wrapper, create, fetchImages, fetchImage, setPage, resetImages;
     beforeEach(() => {
+      fetchImages = jest.fn();
+      fetchImage = jest.fn();
+      setPage = jest.fn();
+      resetImages= jest.fn();
       wrapper = mount(
         <Create
-          fetchImages={jest.fn()}
-          fetchImage={jest.fn()}
+          fetchImages={fetchImages}
+          fetchImage={fetchImage}
+          setPage={setPage}
+          resetImages={resetImages}
           page={2}
-          images={[]}
+          images={[1,2,3]}
           image={{}}
         />,
       );
@@ -21,5 +27,6 @@ describe('Create', () => {
     it('Should render like snapshot', () => {
       expect(wrapper).toMatchSnapshot();
     });
+
   });
 });
