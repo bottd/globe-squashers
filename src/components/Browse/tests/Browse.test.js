8383120e95cbd364a533cf5871c1ffb5f9973b50
Drake Bott <bott.da@gmail.com>
2018-11-06 11:42:21 -0700

    Test Browse MDTP and MSTP

--- src/components/Browse/tests/Browse.test.js
+++ src/components/Browse/tests/Browse.test.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {Browse} from '../';
+import {Browse, mapStateToProps, mapDispatchToProps} from '../';
 
 describe('Browse', () => {
   describe('Browse component', () => {
     it('should render like snapshot', () => {
       const wrapper = shallow(<Browse proofs={[]} />);
       expect(wrapper).toMatchSnapshot();
+    });
+  });
+
+  describe('Map state to props', () => {
+    it('Should return the proofs from state', () => {
+      const mockState = {
+        proofs: [],
+        other: [],
+      };
+      const expected = {
+        proofs: [],
+      };
+      const result = mapStateToProps(mockState);
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('Map dispatch to props', () => {
+    it('Should return an empty object', () => {
+      const result = mapDispatchToProps(jest.fn());
+      expect(result).toEqual({});
     });
   });
 });
