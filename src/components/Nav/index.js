efa5e1889628490c993de533cef5e366e47472a2
Drake Bott <bott.da@gmail.com>
2018-11-29 16:50:20 -0700

    Add globe-squashers to NavLinks

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,13 +5,13 @@ import './Nav.css';
 export default function Nav() {
   return (
     <div className="Nav">
-      <NavLink exact to="/" className="NavLink">
+      <NavLink exact to="/globe-squashers" className="NavLink">
         HOME
       </NavLink>
-      <NavLink to="/create" className="NavLink">
+      <NavLink to="/globe-squashers/create" className="NavLink">
         CREATE
       </NavLink>
-      <NavLink to="/browse" className="NavLink">
+      <NavLink to="/globe-squashers/browse" className="NavLink">
         BROWSE
       </NavLink>
     </div>
